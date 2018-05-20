150794bbc6c7a85c89a3ebf1e36c07286c9d2cb5
aaltamirano1 <angel.g.altam@gmail.com>
2018-05-20 16:18:02 -0400

    add directory of employees using api

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const main = document.querySelector("#main");
+var avatar = document.querySelector('img');
+var fullname = document.querySelector('#name');
+var email = document.querySelector('#email');
+var city = document.querySelector('#city');
+var url = 'https://randomuser.me/api/';
+
+function handleErrors(response){
+  if (!response.ok){
+    throw Error(response.status);
+  }
+  return response;
+}
+function parseJSON(response){
+  return response.json().then(function(data){
+    return data['results'][0];;
+  });
+}
+function addUser(data){
+  main.innerHTML +=`
+    <div class="employee">
+      <img src="`+data.picture.medium+`" class="profile-pic" alt="Profile Pic">
+      <div class="info">
+        <h3 class="name">`+data.name.first + ' ' + data.name.last+`</h3>
+        <p class="email">`+data.email+`</p>
+        <p class="city">`+data.location.city+`</p>
+      </div>
+    </div>`;
+}
+function displayErrors(error){
+  console.log(error);
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+  for(let i=0; i<9; i++){
+    fetch(url)
+    .then(handleErrors)
+    .then(parseJSON)
+    .then(addUser)
+    .catch(displayErrors);
+  }
+
+});

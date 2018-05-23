e9b8056786c922e149095cde744a601105b212fe
Angel Altamirano <angel.g.altam@gmail.com>
2018-05-23 17:12:12 -0400

    switch to axios instead of fetch.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,25 @@
 const main = document.querySelector("#main");
 const url = 'https://randomuser.me/api/';
 
-function handleErrors(response){
-  if (!response.ok){
-    throw Error(response.status);
-  }
-  return response;
-}
-function parseJSON(response){
-  return response.json().then(function(data){
-    return data['results'][0];;
-  });
-}
-function addUser(data){
+function getUser(response){
+  var data = response.data['results'][0];
   main.innerHTML +=`
-    <div class="employee">
-      <img src="`+data.picture.medium+`" class="profile-pic" alt="Profile Pic">
-      <div class="info">
-        <h3 class="name">`+data.name.first + ' ' + data.name.last+`</h3>
-        <p class="email">`+data.email+`</p>
-        <p class="city">`+data.location.city+`</p>
-      </div>
-    </div>`;
-}
-function displayErrors(error){
-  console.log(error);
+  <div class="employee">
+    <img src="`+data.picture.medium+`" class="profile-pic" alt="Profile Pic">
+    <div class="info">
+      <h3 class="name">`+data.name.first + ' ' + data.name.last+`</h3>
+      <p class="email">`+data.email+`</p>
+      <p class="city">`+data.location.city+`</p>
+    </div>
+  </div>`;
 }
 
 document.addEventListener("DOMContentLoaded",()=>{
   for(let i=0; i<9; i++){
-    fetch(url)
-    .then(handleErrors)
-    .then(parseJSON)
-    .then(addUser)
-    .catch(displayErrors);
+     axios.get(url)
+    .then(getUser)
+    .catch(function(error){
+      alert('Oh no, something went wrong.\n\n' + error);
+    });
   }
-
 });

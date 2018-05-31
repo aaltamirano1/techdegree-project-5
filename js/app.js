f9198a7b412dd705e93694e95a8ce2451eb5627a
aaltamirano1 <angel.g.altam@gmail.com>
2018-05-30 20:02:13 -0400

    remove extra code from pull request

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@ function closeModal(){
   document.querySelector("#modal").style.display = 'none';
 }
 
-<<<<<<< HEAD
 function handleErrors(response){
   if (!response.ok){
     throw Error(response.status);
@@ -75,27 +74,3 @@ $(document).ready(function(){
   });
 
 });
-=======
-function getUser(response){
-  var data = response.data['results'][0];
-  main.innerHTML +=`
-  <div class="employee">
-    <img src="`+data.picture.medium+`" class="profile-pic" alt="Profile Pic">
-    <div class="info">
-      <h3 class="name">`+data.name.first + ' ' + data.name.last+`</h3>
-      <p class="email">`+data.email+`</p>
-      <p class="city">`+data.location.city+`</p>
-    </div>
-  </div>`;
-}
-
-document.addEventListener("DOMContentLoaded",()=>{
-  for(let i=0; i<9; i++){
-     axios.get(url)
-    .then(getUser)
-    .catch(function(error){
-      alert('Oh no, something went wrong.\n\n' + error);
-    });
-  }
-});
->>>>>>> bb51d158c5affddedec576119a31defcc03db01f
